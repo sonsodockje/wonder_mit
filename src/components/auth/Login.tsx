cf90273a3fef9b482f2e89b0948f5bb4a4efee2b
sonsodockje <hanaroo0115@naver.com>
2024-05-13 17:02:05 +0900

    Update 회원가입, 로그인 유효성 검사

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  function isValidEmail(email) {
+    var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  }
+
   function handleLogin(
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ): void {
     e.preventDefault();
     // 여기서 다른 작업을 수행하거나 필요한 함수를 호출할 수 있습니다.
+    return navigate("/");
   }
 
   return (
@@ -12,16 +29,34 @@ export default function Login() {
         type="email"
         placeholder="이메일"
         className="rounded-md h-10 bg-bg-200 px-4  text-sm"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
-      <input
-        type="password"
-        placeholder="비밀번호"
-        className="rounded-md h-10 bg-bg-200 px-4  text-sm"
-      />
+      {!isValidEmail(email) && email.length ? (
+        <p className="text-gray-dark text-xs ml-3 mb-3">
+          이메일 형식이 아닙니다.
+        </p>
+      ) : null}
+      <div className="relative">
+        <input
+          type={showPassword ? "text" : "password"}
+          placeholder="비밀번호"
+          className="rounded-md h-10 bg-bg-200 px-4  w-full text-sm"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p
+          onClick={handleClickShowPassword}
+          className="cursor-pointer absolute right-4 top-2.5 text-gray text-sm"
+        >
+          {showPassword ? "비밀번호 가리기" : "비밀번호 보기"}
+        </p>
+      </div>
 
       <button
         onClick={(e) => handleLogin(e)}
-        className="bg-primary-200 text-bg-100 rounded-md h-10 "
+        className="bg-primary-200 text-bg-100 rounded-md h-10 disabled:cursor-not-allowed disabled:bg-gray-light"
+        disabled={!isValidEmail(email)}
       >
         로그인
       </button>
--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -1,17 +1,39 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
+  const navigate = useNavigate();
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password1, setPassword1] = useState("");
   const [password2, setPassword2] = useState("");
 
+  function isValidEmail(email: string) {
+    var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  }
+  function isSame(password1: string, password2: string) {
+    if (password1 === password2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
   function handleSignup(
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ): void {
     e.preventDefault();
-    // 여기서 다른 작업을 수행하거나 필요한 함수를 호출할 수 있습니다.
+    return navigate("auth?mode=login");
   }
+
+  // console.log(
+  //   isValidEmail(email) &&
+  //     isSame(password1, password2) &&
+  //     password2.length > 1 &&
+  //     name.length > 1
+  // );
+
   return (
     <>
       <input
@@ -21,6 +43,7 @@ export default function Signup() {
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
+
       <input
         type="text"
         className="rounded-md h-10 bg-bg-200 px-4"
@@ -28,6 +51,11 @@ export default function Signup() {
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
+      {!isValidEmail(email) && email.length ? (
+        <p className="text-gray-dark text-xs ml-3 mb-3">
+          이메일 형식이 아닙니다.
+        </p>
+      ) : null}
       <input
         type="password"
         className="rounded-md h-10 bg-bg-200 px-4"
@@ -35,6 +63,11 @@ export default function Signup() {
         value={password1}
         onChange={(e) => setPassword1(e.target.value)}
       />
+      {password1.length < 8 && password1.length ? (
+        <p className="text-gray-dark text-xs ml-3 mb-3">
+          8자리 이상 입력해주세요.
+        </p>
+      ) : null}
       <input
         type="password"
         className="rounded-md h-10 bg-bg-200 px-4"
@@ -42,9 +75,42 @@ export default function Signup() {
         value={password2}
         onChange={(e) => setPassword2(e.target.value)}
       />
+      {!isSame(password1, password2) && password2.length ? (
+        <p className="text-gray-dark text-xs ml-3 mb-3">
+          비밀번호가 일치하지 않습니다.
+        </p>
+      ) : null}
+
+      {/* {isValidEmail(email) &&
+      isSame(password1, password2) &&
+      password2.length > 1 &&
+      name.length > 1 ? (
+        <button
+          onClick={(e) => handleSignup(e)}
+          className="bg-primary-200 text-bg-100 rounded-md h-10"
+        >
+          가입
+        </button>
+      ) : (
+        <button
+          disabled
+          className="bg-gray-light cursor-not-allowed text-bg-100 rounded-md h-10 disabled:"
+        >
+          가입
+        </button>
+      )} */}
+
       <button
         onClick={(e) => handleSignup(e)}
-        className="bg-primary-200 text-bg-100 rounded-md h-10"
+        className="bg-primary-200 text-bg-100 rounded-md h-10 disabled:cursor-not-allowed disabled:bg-gray-light"
+        disabled={
+          !(
+            isValidEmail(email) &&
+            isSame(password1, password2) &&
+            password2.length > 1 &&
+            name.length > 1
+          )
+        }
       >
         가입
       </button>
