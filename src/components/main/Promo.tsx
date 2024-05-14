3d0bc714bd52bea5350adc18d50eb37c7d8c0f3d
sonsodockje <hanaroo0115@naver.com>
2024-05-15 01:18:14 +0900

    Update Swiper 자동 넘기기

--- src/components/main/Promo.tsx
+++ src/components/main/Promo.tsx
@@ -1,19 +1,31 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import {  Autoplay } from 'swiper/modules';
 import "swiper/css";
+import 'swiper/css/autoplay';
+
 
 export default function Promo() {
   const data = [
     {
       name: "test",
+      img: "https://images.unsplash.com/photo-1643431452454-1612071b0671?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
       name: "test2",
+      img:"https://images.unsplash.com/photo-1541082515984-c0afd8f37f73?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      name: "test3",
+      img:"https://images.unsplash.com/photo-1525533366436-e69c6bfe2073?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D"
     },
   ];
 
   return (
     <div className="overflow-hidden rounded-lg h-auto">
-      <Swiper>
+      <Swiper
+       modules={[Autoplay]}
+       autoplay={{delay:3000}}
+      >
         {data.map((data) => (
           <SwiperSlide>
             <PromoCard data={data} />
@@ -26,8 +38,11 @@ export default function Promo() {
 
 function PromoCard({ data }: any) {
   return (
-    <div className="bg-color-pink1 p-5 h-[300px]">
-      <p>{data.name}</p>
+    <div className=" h-[300px] relative">
+      <img src={data.img} alt="" className="w-full "/>
+      <div className="absolute p-5 top-0 z-999 bg-color-pink3">
+        <p>{data.name}</p>
+      </div>
     </div>
   );
 }
--- src/components/main/UserSimpleInfo.tsx
+++ src/components/main/UserSimpleInfo.tsx
@@ -17,9 +17,26 @@ export default function UserSimpleInfo() {
     const userData = JSON.parse(userDataString);
     // console.log(userData);
     return (
-      <div className="p-5 h-[300px] bg-color-white rounded-lg shadow-lg">
-        <p>{userData.displayName} 님</p>
-        <img src={userData.photoURL} alt="" />
+      <div className="p-5 h-[300px] bg-color-white gap-8 flex-col rounded-lg shadow-lg flex items-center justify-center">
+        <div className="flex gap-3 items-center">
+          <div className="w-[120px] h-[120px] border-2 border-color-point-pink rounded-full bg-color-blue overflow-hidden"> 
+            <img src={userData.photoURL} alt="" className="w-full h-full object-cover object-center" />
+          </div>
+          <div className="flex flex-col">    
+              <p>{userData.displayName}</p>
+              <p>#체육 #주짓수</p>  
+              <p>운동 수집 전문가</p>
+          </div>
+        </div>
+        <div className="flex gap-2 flex-col w-full">
+          <p className="w-full text-center py-2 border-2 border-color-point-pink text-sm  rounded-lg font-semibold
+ text-color-point-pink "
+          >즐겨찾기 행사 11개</p>
+          <p className=" w-full text-center py-2 border-2 border-color-point-pink text-sm rounded-lg font-semibold
+ text-color-point-pink"
+          >참가한 행사 11개</p>
+        </div>
+       
       </div>
     );
   }
